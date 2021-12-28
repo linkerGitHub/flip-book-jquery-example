7fa5dfccbd2cf40f065220c999722b703c453690
junhao.kuang <linker-junhao@outlook.com>
2021-12-29 00:05:35 +0800

    fix: 增加最大图片限制

--- mobile-viewer/viewer.js
+++ mobile-viewer/viewer.js
@@ -22,7 +22,7 @@ if (!pdfjsLib.getDocument || !pdfjsViewer.PDFViewer) {
 
 const USE_ONLY_CSS_ZOOM = true;
 const TEXT_LAYER_MODE = 0; // DISABLE
-const MAX_IMAGE_SIZE = 1024 * 1024;
+const MAX_IMAGE_SIZE = 1024 * 1024 * 30;
 const CMAP_URL = "../node_modules/pdfjs-dist/cmaps/";
 const CMAP_PACKED = true;
 
